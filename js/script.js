0ce62a434f3e0e3d48d7c40c2ecccfa4ee9b9561
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-05-07 01:32:29 +0000

    huh

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall updated by Anna S.
-// Created on: May 2025
+// Created by: Mr. Coxall
+// Created on: Sep 2020
 // This file contains the JS functions for index.html
-
-"use strict"
